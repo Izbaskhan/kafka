5b376d4f1e11394a80901cf7f459dfa596d44d7e
Избасхан Ябеков <izbaskhan@yandex.kz>
2022-05-21 18:57:40 +0600

    Пример использования Faust

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 import faust
 
-
-class Greeting(faust.Record):
-    from_name: str
-    to_name: str
+app = faust.App(id="test", broker="kafka://10.82.1.25:9092", store="memory://")
 
 
-app = faust.App('hello-app', broker='kafka://localhost')
-topic = app.topic('hello-topic', value_type=Greeting)
-
-
-@app.agent(topic)
-async def hello(greetings):
-    async for greeting in greetings:
-        print(f'Hello from {greeting.from_name} to {greeting.to_name}')
-
-
-@app.timer(interval=1.0)
-async def example_sender(app):
-    await hello.send(
-        value=Greeting(from_name='Faust', to_name='you'),
-    )
-
-
-if __name__ == '__main__':
+# convenience func for launching the app
+def main() -> None:
     app.main()
+
+
+# Input topic, NOT managed by Faust. Marked
+input_topic = app.topic('input', internal=False, partitions=1, value_type=str)
+
+# Faust will create this topic for us.
+colors_topic = app.topic('colors_topic', internal=True, partitions=1, value_type=str)
+
+# Output, also NOT managed by Faust.
+output_topic = app.topic('output', internal=False, partitions=1, value_type=str)
+
+# Let's define a table to keep the count of valid RGB colors.
+colors_count_table = app.Table('colors-count', key_type=str, value_type=int, partitions=1, default=int)
+
+VALID_WORDS = ["red", "green", "blue"]
+
+
+@app.agent(input_topic)
+async def filter_colors(words):
+    async for word in words:
+        print(word)
+        if word in VALID_WORDS:
+            await colors_topic.send(value=word)
+
+
+@app.agent(colors_topic)
+async def colors_count(colors):
+    async for color in colors:
+        colors_count_table[color] += 1
+        print(f'{color} has now been seen {colors_count_table[color]} times')
+        await output_topic.send(value=colors_count_table[color])
